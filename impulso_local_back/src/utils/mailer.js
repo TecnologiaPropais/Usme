9688419bb5705331f5c644f712c3abb484ef8117
RoG777 <dfmartinezfa@gmail.com>
2025-09-09 12:14:55 -0500

    feat: configurar sistema de correos automáticos para subsanación
    - Implementar envío automático de correos cuando cambia estado a subsanación
    - Agregar variables dinámicas en templates de correo (nombre_persona, id_inscripcion, etc.)
    - Configurar link de subsanación con ID del registro
    - Integrar con tabla estado_notificacion para templates personalizables

--- impulso_local_back/src/utils/mailer.js
+++ impulso_local_back/src/utils/mailer.js
@@ -5,14 +5,14 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'Outlook',
   auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   }
 });
 
 async function enviarCorreo({ to, subject, text }) {
   await transporter.sendMail({
-    from: `"Propais Notificaciones" <${process.env.MAIL_USER}>`,
+    from: `"Propais Notificaciones" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     text
